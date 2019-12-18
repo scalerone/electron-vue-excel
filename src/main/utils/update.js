cda0b4f226b3c01589d4939aef921d070fd42f33
wuhui <wuhui@yadingdata.com>
2019-12-18 11:08:52 +0800

    加入日志，打包后可通过文体查看日志

--- src/main/utils/update.js
+++ src/main/utils/update.js
@@ -1,9 +1,12 @@
 import {autoUpdater} from 'electron-updater'
 import {ipcMain} from 'electron'
+let log = require('electron-log');
+log.transports.file.level = 'silly';
+log.transports.console.level = false;
 let mainWindow = null;
 export function updateHandle(window, feedUrl) {
     mainWindow = window;
-    let message = {
+    let messagedata = {
         error: '检查更新出错',
         checking: '正在检查更新……',
         updateAva: '检测到新版本，正在下载……',
@@ -13,7 +16,8 @@ export function updateHandle(window, feedUrl) {
     autoUpdater.setFeedURL(feedUrl);
     //监听升级失败事件
     autoUpdater.on('error', function (error) {
-        console.log(error)
+        log.info('autoUpdater error:')
+        log.info(error)
         sendUpdateMessage({
             cmd: 'error',
             message: error
@@ -21,6 +25,9 @@ export function updateHandle(window, feedUrl) {
     });
     //监听开始检测更新事件
     autoUpdater.on('checking-for-update', function (message) {
+        log.info('autoUpdater checking-for-update:')
+        log.info(message)
+        console.log(message)
         sendUpdateMessage({
             cmd: 'checking-for-update',
             message: message
@@ -28,6 +35,8 @@ export function updateHandle(window, feedUrl) {
     });
     //监听发现可用更新事件
     autoUpdater.on('update-available', function (message) {
+        log.info('autoUpdater update-available:')
+        log.info(message)
         sendUpdateMessage({
             cmd: 'update-available',
             message: message
@@ -35,6 +44,8 @@ export function updateHandle(window, feedUrl) {
     });
     //监听没有可用更新事件
     autoUpdater.on('update-not-available', function (message) {
+        log.info('autoUpdater update-not-available:')
+        log.info(message)
         sendUpdateMessage({
             cmd: 'update-not-available',
             message: message
@@ -48,6 +59,7 @@ export function updateHandle(window, feedUrl) {
     // total : 总大小
     // transferred: 已经下载
     autoUpdater.on('download-progress', function (progressObj) {
+        log.info('autoUpdater download-progress:')
         sendUpdateMessage({
             cmd: 'download-progress',
             message: progressObj
@@ -57,6 +69,16 @@ export function updateHandle(window, feedUrl) {
 
     //监听下载完成事件
     autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl) {
+        log.info('autoUpdater update-downloaded:')
+        log.info(releaseNotes)
+        log.info(releaseName)
+        log.info(releaseDate)
+        log.info(updateUrl)
+        // 下载太快可能无法触发downloadProgress事件，所以手动通知一下
+        sendUpdateMessage({
+            cmd: 'download-progress',
+            message: {percent: 100}
+        })
         sendUpdateMessage({
             cmd: 'update-downloaded',
             message: {
@@ -67,9 +89,10 @@ export function updateHandle(window, feedUrl) {
             }
         })
 
-        console.log("开始更新");
+
         //退出并安装更新包
         ipcMain.on('updateNow', (e, arg) => {
+            log.info('updateNow:')
             //some code here to handle event
             autoUpdater.quitAndInstall();
         })
@@ -79,6 +102,7 @@ export function updateHandle(window, feedUrl) {
     //接收渲染进程消息，开始检查更新
     ipcMain.on("checkForUpdate", (e, arg) => {
         //执行自动更新检查
+        log.info('trigger checkForUpdate ...')
         // sendUpdateMessage({cmd:'checkForUpdate',message:arg})
         autoUpdater.checkForUpdates();
     })
